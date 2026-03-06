7679588203fab43897304bc389000025fad0d81d
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-03-06 16:20:45 +0530

    minor changes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,6 +2,13 @@ const { Router } = require('express');
 
 const authRouter = Router();
 
+/**
+ * @route POST /api/auth/register
+ * @desc Register a new user
+ * @access Public
+ */
+
+
 
 
 module.exports = authRouter;
