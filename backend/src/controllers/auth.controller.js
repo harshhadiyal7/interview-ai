f54acb62f87c2252ad20b07963938e3312c307f7
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-03-06 17:12:16 +0530

    create login and register api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,9 +29,64 @@ async function registerUserController(req, res) {
         email,
         password: hash
      });
-     
- }
+
+    const token = jwt.sign(
+        { id: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: '1d' });
+    
+    res.cookie('token', token)
+    
+    return res.status(201).json({
+        message: 'User registered successfully',
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    });
+}
+
+/**
+ * @name loginUserController
+ * @description Controller to handle user login
+ * @access Public
+ */
+
+async function loginUserController(req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ 
+            message: 'Invalid email or password' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(400).json({ 
+            message: 'Invalid email or password' });
+    }
+
+    const token = jwt.sign(
+        { id: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: '1d' }
+    );
+    res.cookie('token', token);
+
+    return res.status(200).json({
+        message: 'User logged in successfully',
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    });
+}
+
 
 module.exports = {
-    registerUserController
+    registerUserController,
+    loginUserController
 }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -9,6 +9,12 @@ const authRouter = Router();
  */
 authRouter.post('/register', authController.registerUserController)
 
+/**
+ * @route POST /api/auth/login
+ * @desc Login a user with email and password
+ * @access Public
+ */
+authRouter.post('/login', authController.loginUserController)
 
 
 module.exports = authRouter;
