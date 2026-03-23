e1dffffc39a13bc1db6ec8957b1504f7d427116c
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-03-23 12:02:04 +0530

    add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app.routes'
+
+
 
 function App() {
  
   return (
    <>
-    
+    <RouterProvider router={router} />
    </>
   )
 }
