40dd0a66a8e1b4053c013dd82eb78e0902de4e89
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-03-23 17:03:59 +0530

    prevent hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app.routes'
-
+import { AuthProvider } from './features/auth/auth.context.jsx'
 
 
 function App() {
  
   return (
-   <>
+   <AuthProvider>
     <RouterProvider router={router} />
-   </>
+   </AuthProvider>
   )
 }
 
--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,15 +1,22 @@
-
+import { useState } from "react"
 import { Link,useNavigate } from "react-router"
 import "../auth.form.scss"
-
+import { useAuth } from "../hooks/useAuth"
 
 const Login = () => {
     const navigate = useNavigate()
+    const {loading, handleLogin } = useAuth()
+    
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        handleLogin({ email, password })
     }
-
+    if(loading) {
+        return <main><h1>Login..........</h1></main>
+    }
     return (
         <main>
             <div className="form-container">
@@ -18,11 +25,13 @@ const Login = () => {
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
                         <input
+                            onChange={(e) => {setEmail(e.target.value)}}
                             type="email" id="email" name='email' placeholder='Enter email address' />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
                         <input
+                            onChange={(e) => {setPassword(e.target.value)}}
                             type="password" id="password" name='password' placeholder='Enter password' />
                     </div>
                     <button className='button primary-button' >Login</button>
